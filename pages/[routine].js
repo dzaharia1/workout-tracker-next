0ad7b53a124f23124bd967f1e2c1a6d17274a990
Dan Zaharia <zaharia.danny@gmail.com>
2023-10-10 17:16:03 -0400

    continue fixing routing

--- pages/[routine].js
+++ pages/[routine].js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 import { Router, useRouter } from 'next/router';
 import Head from 'next/head';
 import Image from 'next/image';
-import Navigation from '../../components/Navigation';
-import SetList from '../../components/SetList';
+import Navigation from '../components/Navigation';
+import SetList from '../components/SetList';
 
 export default function Home({ workoutData, routineData, thisRoutine, nextRoutine, today, apiUrl }) {
   const router = useRouter();
