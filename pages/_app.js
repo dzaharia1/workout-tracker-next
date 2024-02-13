0100f59580c6488fa100d8b1cfee972ec4ec9811
Dan Zaharia <zaharia.danny@gmail.com>
2024-02-12 19:41:40 -0500

    correct definition of Atkinson font

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // import '../styles/utilities/atkinson.scss';
 import '../styles/globals.scss';
-import { Atkinson_Hyperlegible } from '@next/font/google';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
